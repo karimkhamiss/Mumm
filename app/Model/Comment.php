28fc7dd529c29f1289f303251180226a9b7f3a62
Karim <karim.khamiss@gmail.com>
2018-08-01 05:34:48 +0200

    Add Comment

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use App\User;
 use Illuminate\Database\Eloquent\Model;
+use DateTime;
 
 class Comment extends Model
 {
@@ -18,4 +19,39 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class);
     }
+    private  function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+    public function getDateAttribute()
+    {
+
+        $data = $this->time_elapsed_string($this->created_at);
+        return $data;
+    }
+
 }
--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -2,37 +2,78 @@
 
 @section('style')
     <style>
-        .article .content
-        {
+        .article .content {
             padding: 20px;
         }
-        .article .title
+        .article .content p
         {
+            font-size: 18px;
+            margin: 40px 0;
+            margin-top: 20px;
+            line-height: 1.7;
+        }
+
+        .article .title {
             font-size: 30px;
             font-weight: 700;
         }
-        .article .info i
-        {
+
+        .article .info i {
             width: auto;
             height: auto;
             line-height: normal;
             margin: 0;
-            margin-right:10px;
+            margin-right: 10px;
             background: none;
-            color:#888;
+            color: #888;
         }
-        .article .info span
-        {
-            color:#888;
+
+        .article .info span {
+            color: #888;
             margin: 20px 0;
             margin-right: 15px;
             margin-bottom: 30px;
             font-size: 15px;
         }
-        .article
+        .article .article-comments
         {
-
+            width:100%;
+            margin: 20px auto;
         }
+        .article .article-comments .comment
+        {
+            border-bottom:4px solid #ddd;
+            padding:20px;
+            margin-bottom: 10px;
+            background: #fff;
+        }
+        .article .article-comments .comment .comment-head .user-photo img
+        {
+            width:60px;
+            height: 60px;
+            border-radius: 50%;
+        }
+        .article .article-comments .comment .comment-head .user-name
+        {
+            font-size: 17px;
+            line-height: 60px;
+            padding-left: 10px;
+            font-weight: 700;
+        }
+        .article .article-comments .comment .comment-head .date
+        {
+            color:#aaa;
+            font-size:16px;
+            font-weight: 700;
+            font-style: italic;
+        }
+        .article .article-comments .comment .comment-body
+        {
+            padding: 10px;
+            font-size: 17px;
+            color:#555;
+        }
+
     </style>
 @endsection
 
@@ -53,7 +94,7 @@
                 @endif
             </div>
             <div class="content text-left">
-            <h3 class="title text-left">{{$article->title}}</h3>
+                <h3 class="title text-left">{{$article->title}}</h3>
                 <div class="info">
                     <span><i class="fa fa-clock"></i> {{$article->date}}</span>
                     <span><i class="fa fa-user"></i> {{$article->user->name}}</span>
@@ -63,23 +104,42 @@
                 <p class="lead">
                     {{$article->body}}
                 </p>
+                <div class="article-comments">
+                    @foreach($article->comments as $comment)
+                        <div class="comment">
+                            <div class="comment-head">
+                                <div class="user-photo fl-left">
+                                    @if($comment->user->picture)
+                                        <img src="{{$comment->user->picture}}" alt="">
+                                    @else
+                                        <img src="{{asset('images/user.jpg')}}" alt="">
+                                    @endif
+                                </div>
+                                <div class="user-name fl-left">
+                                    {{$comment->user->name}}
+                                </div>
+                                <div class="date fl-right">
+                                    {{$comment->date}}
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="comment-body">
+                                {{$comment->body}}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="leave-comment text-left">
+                    <h3>Feel Free to Give Us Your Feedback</h3>
+                    @include('forms.comments.add')
+                </div>
             </div>
-            <div class="comments">
-                @foreach($article->comments as $comment)
-
-                @endforeach
-
-            </div>
-            <div class="leave-comment text-left">
-                <h3>Feel Free to Give Us Your Feedback</h3>
-                @include('forms.comments.add')
             </div>
         </div>
-    </div>
-@endsection
-@section('script')
+        @endsection
+        @section('script')
 
-    <script src="{{ asset('Ajax/articles.js')}}"></script>
-    <script src="{{ asset('Ajax/comments.js')}}"></script>
+            <script src="{{ asset('Ajax/articles.js')}}"></script>
+            <script src="{{ asset('Ajax/comments.js')}}"></script>
 
 @endsection
